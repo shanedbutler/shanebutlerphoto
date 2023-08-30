42cf44e44527d453ed7a2e5cd53200872e02ee7e
Shane Butler <shanedbutler@gmail.com>
2023-08-30 14:36:07 -0400

    Update navbar to use react-router-dom Link

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -3,37 +3,33 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <>
-      <div className="w-full h-20 sticky top-0">
-        <div className="container mx-auto px-4 h-full">
-          <div className="flex justify-between items-center h-full">
-            Shane Butler
-            <ul className="hidden md:flex gap-x-6 text-black">
-              <li>
-                <Link href="/about">
-                  <p>About</p>
-                </Link>
-              </li>
-              <li>
-                <Link href="/architectural-interiors">
-                  <p>Interiors</p>
-                </Link>
-              </li>
-              <li>
-                <Link href="/point-of-sale">
-                  <p>Point of Sale</p>
-                </Link>
-              </li>
-              <li>
-                <Link href="/portraiture">
-                  <p>People</p>
-                </Link>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </div>
-    </>
+    <div className="flex justify-between items-center h-full">
+      <Link to="/">
+        Shane Butler
+      </Link>
+      <ul className="hidden md:flex gap-x-6 text-black">
+        <li>
+          <Link to="/about">
+            <p>About</p>
+          </Link>
+        </li>
+        <li>
+          <Link to="/architectural-interiors">
+            <p>Interiors</p>
+          </Link>
+        </li>
+        <li>
+          <Link to="/point-of-sale">
+            <p>Point of Sale</p>
+          </Link>
+        </li>
+        <li>
+          <Link to="/portraiture">
+            <p>People</p>
+          </Link>
+        </li>
+      </ul>
+    </div>
   );
 };
 
