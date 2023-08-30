fd98422822eb3d885317d34d356adbefd02d2837
Shane Butler <shanedbutler@gmail.com>
2023-08-30 16:22:33 -0400

    Add swiper library and initialize

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { register } from 'swiper/element/bundle';
+
+// register Swiper custom elements
+register();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
