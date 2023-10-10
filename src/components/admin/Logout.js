16b482731fb41cd2385acdcfa37f0f0bd5ee1cde
Shane Butler <shanedbutler@gmail.com>
2023-10-10 16:03:19 -0400

    Add styling for logout component

--- src/components/admin/Logout.js
+++ src/components/admin/Logout.js
@@ -1,8 +1,36 @@
 export const Logout = ({ onLogout }) => {
     return (
-        <div>
-            <h2>Are you sure you want to log out?</h2>
-            <button onClick={onLogout}>Yes</button>
+        <div className="flex justify-center py-4 px-4 sm:px-6 lg:px-8">
+            <div className="w-full max-w-md space-y-4">
+                <div>
+                    <h2 className="mt-3 text-center text-2xl font-bold tracking-tight text-gray-900">
+                        Logout Admin?
+                    </h2>
+                </div>
+                <div>
+                    <button
+                        onClick={onLogout}
+                        className="group relative flex w-full justify-center rounded-md border border-transparent bg-violet-300 py-2 px-4 text-sm font-medium text-black shadow-sm hover:bg-violet-200 focus:bg-violet-200"
+                    >
+                        <span className="absolute inset-y-0 left-0 flex items-center pl-3">
+                            <svg
+                                className="h-5 w-5 text-indigo-500 group-hover:text-indigo-400"
+                                xmlns="http://www.w3.org/2000/svg"
+                                viewBox="0 0 20 20"
+                                fill="currentColor"
+                                aria-hidden="true"
+                            >
+                                <path
+                                    fillRule="evenodd"
+                                    d="M10 1a4.5 4.5 0 00-4.5 4.5V9H5a2 2 0 00-2 2v6a2 2 0 002 2h10a2 2 0 002-2v-6a2 2 0 00-2-2h-.5V5.5A4.5 4.5 0 0010 1zm3 8V5.5a3 3 0 10-6 0V9h6z"
+                                    clipRule="evenodd"
+                                />
+                            </svg>
+                        </span>
+                        Yes
+                    </button>
+                </div>
+            </div>
         </div>
     );
 };
