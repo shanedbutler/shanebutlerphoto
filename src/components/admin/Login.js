61b95171066a0357a4ec36657f98eaa17dcfdec9
Shane Butler <shanedbutler@gmail.com>
2023-10-11 13:35:53 -0400

    Change lock icon from svg to jsx

--- src/components/admin/Login.js
+++ src/components/admin/Login.js
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { browserLocalPersistence, browserSessionPersistence, sendPasswordResetEmail, setPersistence, signInWithEmailAndPassword } from "firebase/auth";
 import { ForgotPasswordModal } from './ForgotPasswordModal';
+import { LockClosedIcon } from '@heroicons/react/24/solid';
 
 export const Login = ({ auth }) => {
     const [modalOpen, setModalOpen] = useState(false);
@@ -123,19 +124,10 @@ export const Login = ({ auth }) => {
                                 className="group relative flex w-full justify-center rounded-md border border-transparent bg-violet-300 py-2 px-4 text-sm font-medium text-black shadow-sm hover:bg-violet-200 focus:bg-violet-200"
                             >
                                 <span className="absolute inset-y-0 left-0 flex items-center pl-3">
-                                    <svg
+                                    <LockClosedIcon
                                         className="h-5 w-5 text-indigo-500 group-hover:text-indigo-400"
-                                        xmlns="http://www.w3.org/2000/svg"
-                                        viewBox="0 0 20 20"
-                                        fill="currentColor"
                                         aria-hidden="true"
-                                    >
-                                        <path
-                                            fillRule="evenodd"
-                                            d="M10 1a4.5 4.5 0 00-4.5 4.5V9H5a2 2 0 00-2 2v6a2 2 0 002 2h10a2 2 0 002-2v-6a2 2 0 00-2-2h-.5V5.5A4.5 4.5 0 0010 1zm3 8V5.5a3 3 0 10-6 0V9h6z"
-                                            clipRule="evenodd"
-                                        />
-                                    </svg>
+                                    />
                                 </span>
                                 Submit
                             </button>
