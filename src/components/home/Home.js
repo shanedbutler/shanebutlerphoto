37d63b44306d6ab63c527fc8092d38bc24083077
Shane Butler <shanedbutler@gmail.com>
2023-10-12 08:20:01 -0400

    Refactor home component to implement new utils fn

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,27 +1,28 @@
+import { useEffect, useState } from "react";
 import { Gallery } from "../gallery/Gallery";
+import { getDownloadURLs } from "../../utils/storageUtils";
 
-export const Home = () => {
-
-    const images = [
-        {
-            url: "https://res.cloudinary.com/dgxomkis0/image/upload/v1693425391/home/shane_butler_photography_home_2_tvisg4.jpg",
-            alt: "Modern Residential Interior Living Room"
-        },
-        {
-            url: "https://res.cloudinary.com/dgxomkis0/image/upload/v1693425392/home/shane_butler_photography_home_1_hohcis.jpg",
-            alt: "Contemporary Residential Interior Kitchen"
-        },
-    ];
+export const Home = ({ storage }) => {
+    const [images, setImages] = useState([]);
 
     const params = {
         autoplay: true,
         keyboard: true,
         loop: true,
         spaceBetween: 25,
-        speed: 500,
         autoHeight: true,
     };
-    
+
+    useEffect(() => {
+        // Call the getDownloadURLs function to get the array of image URLs
+        getDownloadURLs(storage, 'home').then((imageUrls) => {
+            // Use the array of image URLs as needed
+            setImages(imageUrls);
+        }).catch((error) => {
+            console.error(error);
+        });
+    }, []);
+
     return (
         <Gallery images={images} params={params} />
     );
