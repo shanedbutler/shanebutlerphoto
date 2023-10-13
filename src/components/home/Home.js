507e060cee6f12eaeb3267436c31ce4d8bd8c37b
Shane Butler <shanedbutler@gmail.com>
2023-10-13 16:00:26 -0400

    Refactor to pass on props

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -3,8 +3,6 @@ import { Gallery } from "../gallery/Gallery";
 import { getDownloadURLs } from "../../utils/storageUtils";
 
 export const Home = ({ storage }) => {
-    const [images, setImages] = useState([]);
-
     const params = {
         autoplay: true,
         keyboard: true,
@@ -13,17 +11,7 @@ export const Home = ({ storage }) => {
         autoHeight: true,
     };
 
-    useEffect(() => {
-        // Call the getDownloadURLs function to get the array of image URLs
-        getDownloadURLs(storage, 'home').then((imageUrls) => {
-            // Use the array of image URLs as needed
-            setImages(imageUrls);
-        }).catch((error) => {
-            console.error(error);
-        });
-    }, []);
-
     return (
-        <Gallery images={images} params={params} />
+        <Gallery storagePath="home" storage={storage} params={params} />
     );
 }
