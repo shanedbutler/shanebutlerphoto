e9575f432698621e1d97b252e6226cf43445686c
Shane Butler <shanedbutler@gmail.com>
2023-10-15 15:14:26 -0400

    Remove unused prop

--- src/index.js
+++ src/index.js
@@ -34,7 +34,7 @@ const supabase = createClient(url, apiKey);
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App app={app} supabase={supabase} />
+    <App supabase={supabase} />
   </React.StrictMode>
 );
 
